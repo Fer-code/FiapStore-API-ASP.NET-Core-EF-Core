bb3824af13d75302d7b0d4f7a8eedcdb7bfa1b15
Fernanda Rocha Miranda <fernandamirandacursos@gmail.com>
2025-05-01 13:50:42 -0300

    repositories

---
+++ Infrastructure/Repository/LivroRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entity;
+using Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class LivroRepository : EFRepository<Livro>, ILivroRepository
+    {
+        public LivroRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
